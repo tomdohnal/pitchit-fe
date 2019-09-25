5507d35636c300572613b4f8eeeff395351de134
Petr Volny <volny.petr@gmail.com>
2019-09-25 15:11:45 +0200

    Button component v1 + Gaals screen stub

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './Home'
 import Callback from './Callback'
 import Welcome from './Welcome'
+import Goals from './Goals'
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route exact path="/" component={Home} />
                 <Route path="/callback" component={Callback} />
                 <Route path="/welcome" component={Welcome} />
+                <Route path="/goals" component={Goals} />
                 {/* <Route path="/about" component={About} />
                 <Route path="/topics" component={Topics} /> */}
             </div>

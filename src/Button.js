f1d8c0769c1d1c8131b058c749add5c5d0750d82
Tom Dohnal <tomas.dohnal@gmail.com>
2019-09-26 13:49:09 +0200

    button sound

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,21 @@
-import React  from 'react'
+import React from 'react'
 import css from './button.module.sass'
-
 
 const Button = ({ children, disabled, onClick }) => {
     const className = [css.button]
     return (
-        <button type='button' className={className.join(' ')} onClick={onClick} disabled={disabled}>
+        <button
+            type="button"
+            className={className.join(' ')}
+            onPointerDown={() => {
+                const audio = new Audio('Button Click Off-SoundBible.com-1730098776.mp3')
+                audio.play('')
+            }}
+            onClick={() => {
+                onClick && onClick()
+            }}
+            disabled={disabled}
+        >
             {children}
         </button>
     )
